014c5337fbc61e304c2ef27510971520fb9a4b69
Janine Luk <janineluk95@gmail.com>
2018-11-13 12:19:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,6 @@
 def oxford_comma(array)
   if array.length == 1 
     return array.join
-  else array.length == 2
-    return array.join(" and ")
   else array.length >= 3 
     return array[0..-2].join(", ") + ", and " + array[-1]
   end
